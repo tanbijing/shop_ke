47ea2417cd19e7761e0dae5567cd264aac1eb004
tanbijing <562898180@qq.com>
2016-03-01 18:22:41 +0800

    test HTTP get

--- shop_ke/ActivityViewController.swift
+++ shop_ke/ActivityViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import Alamofire
 
 class ActivityViewController: UIViewController {
-
+    @IBOutlet weak var bannerSv: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadData()
+        
         // Do any additional setup after loading the view.
     }
 
@@ -19,6 +23,32 @@ class ActivityViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loadData(){
+        var params:Dictionary<String,AnyObject> = Dictionary()
+        params["client_type"] = "iphone"
+        params["num"] = "4"
+        params["pa"] = "pa"
+        Alamofire.request(.GET, API_URL+"/brand_theme_index", parameters: params)
+            .responseJSON { response in
+                print("==============1")
+                print(response.request)  // original URL request
+                print("==============2")
+                print(response.response) // URL response
+                print("==============3")
+                print(response.data)     // server data
+                print("==============4")
+                print(response.result)   // result of response serialization
+                print("==============5")
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+    }
+    
+    
+    
+    
     
     /*
     // MARK: - Navigation
