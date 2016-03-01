72f45cadf4a8c55d5e4f419fc8e78bdf38fa7f1f
mac <mac@macdeMacBook-Pro-2.local>
2016-03-01 09:50:47 +0800

    my-UI

---
+++ shop_ke/loginViewController.swift
@@ -0,0 +1,51 @@
+//
+//  loginViewController.swift
+//  shop_ke
+//
+//  Created by mac on 16/2/29.
+//  Copyright © 2016年 peraytech. All rights reserved.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBOutlet weak var account: UITextField!
+
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func register(sender: UIButton) {
+        
+    }
+    
+    @IBAction func loginBtn(sender: UIButton) {
+        
+    }
+    
+    @IBAction func back(sender: UIButton) {
+        let nib=MeViewController()
+        self.presentViewController(nib, animated:true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
