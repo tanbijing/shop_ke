3b23791d7352707ee09a79128fb9ac022d97fe41
tanbijing <562898180@qq.com>
2016-03-04 17:59:28 +0800

    banner loop edit

--- shop_ke/ActivityViewController.swift
+++ shop_ke/ActivityViewController.swift
@@ -14,6 +14,7 @@ class ActivityViewController: UIViewController,UIScrollViewDelegate {
     @IBOutlet weak var pageControl: UIPageControl!
     
     var activities:[Activity] = []
+    var showBannerActivities:[Activity] = []
     var bannerTime:NSTimer? = nil
     
     override func viewDidLoad() {
@@ -30,11 +31,31 @@ class ActivityViewController: UIViewController,UIScrollViewDelegate {
     }
     
     func scrollViewDidScroll(scrollView: UIScrollView) {
-//        print(scrollView.contentOffset.x)
         if scrollView != self.bannerSv{
             return
         }
-        self.pageControl.currentPage = Int((scrollView.contentOffset.x-scrollView.frame.width)/scrollView.frame.width) + 1
+        let x = scrollView.contentOffset.x;
+        let currentPage = self.pageControl.currentPage
+        //下一张
+        if (x >= 2 * scrollView.frame.size.width)
+        {
+            if(currentPage==self.activities.count-1){
+                self.pageControl.currentPage = 0;
+            }else{
+                self.pageControl.currentPage += 1
+            }
+            self.refreshBannerDatas()
+        }
+        //上一张
+        if (x <= 0)
+        {
+            if(currentPage==0){
+                self.pageControl.currentPage = self.activities.count-1;
+            }else{
+                self.pageControl.currentPage -= 1
+            }
+            self.refreshBannerDatas()
+        }
     }
     
     func loadData(){
@@ -49,16 +70,16 @@ class ActivityViewController: UIViewController,UIScrollViewDelegate {
                 print(failData)
         }
     }
-    
+    //第一次加载banner数据
     func loadBanner(){
-        print(self.activities.count)
         for image in self.bannerSv.subviews{
             image.removeFromSuperview()
         }
         self.pageControl.numberOfPages = self.activities.count
         self.bannerSv.contentSize = CGSizeMake(self.bannerSv.frame.width*CGFloat(self.activities.count), self.bannerSv.frame.height)
+        self.changeBunnerShowDatas()
         var pic_index = 0
-        for activity in activities{
+        for activity in self.showBannerActivities{
             let image = UIImageView.init(frame: CGRectMake(self.bannerSv.frame.width*CGFloat(pic_index), 0, self.bannerSv.frame.width, self.bannerSv.frame.height))
             if let activityImageUrl = activity.image_path {
                 image.setWebImage(activityImageUrl , placeHolder: UIImage.init(named: "w_icon"))
@@ -66,27 +87,50 @@ class ActivityViewController: UIViewController,UIScrollViewDelegate {
             self.bannerSv.addSubview(image)
             pic_index += 1
         }
+        self.bannerSv.setContentOffset(CGPointMake(self.bannerSv.frame.size.width, 0), animated: false)
         bannerTime = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: Selector("loopBannerImage"), userInfo: nil, repeats: true)
     }
-    
+    //bunner滚动定时器
     func loopBannerImage(){
         UIView.animateWithDuration(0.7, animations: { () -> Void in
-                self.bannerSv.setContentOffset(CGPointMake(self.bannerSv.contentOffset.x+self.bannerSv.frame.size.width, 0), animated: true)
+                self.bannerSv.setContentOffset(CGPointMake(self.bannerSv.frame.size.width*CGFloat(2), 0), animated: true)
             }) { (result) -> Void in
-                print(self.pageControl.currentPage)
-                if self.pageControl.currentPage==4{
-                    self.bannerSv.setContentOffset(CGPointMake(0, 0), animated: true)
-                }
+                print("buner定时滚动:\(result)")
         }
-//        [UIView beginAnimations:nil context:NULL];
-//        [UIView setAnimationDuration:0.7f];
-//        [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
-//        [sv setContentOffset:CGPointMake(rect.size.width * 2, 0.0f) animated:YES];
-//        [UIView commitAnimations];
     }
-    
-    
-    
+    //滚动到scrollView的端点后刷新bunner
+    func refreshBannerDatas(){
+        self.changeBunnerShowDatas()
+        var scrollImages = self.bannerSv.subviews as! [UIImageView]
+        var activity_index = 0
+        for activity in self.showBannerActivities {
+            if let activityImageUrl = activity.image_path {
+                scrollImages[activity_index].setWebImage(activityImageUrl , placeHolder: UIImage.init(named: "w_icon"))
+                activity_index += 1
+            }
+        }
+        self.bannerSv.setContentOffset(CGPointMake(self.bannerSv.frame.size.width,0), animated: false)
+    }
+    //根据当前的page改变要显示的bunner数据
+    func changeBunnerShowDatas(){
+        let currentPage = self.pageControl.currentPage
+        switch currentPage{
+            case 0:
+                self.setShowActivities(self.activities.count-1, currentIndex: currentPage, lastIndex: currentPage+1)
+            case self.activities.count-1:
+                self.setShowActivities(currentPage-1, currentIndex: currentPage, lastIndex: 0)
+            default:
+                self.setShowActivities(currentPage-1, currentIndex: currentPage, lastIndex: currentPage+1)
+        }
+    }
+    //重置bunner显示的活动数组
+    func setShowActivities(firstIndex:Int,currentIndex:Int,lastIndex:Int){
+        print(self.activities.count)
+        self.showBannerActivities.removeAll()
+        self.showBannerActivities.append(self.activities[firstIndex])
+        self.showBannerActivities.append(self.activities[currentIndex])
+        self.showBannerActivities.append(self.activities[lastIndex])
+    }
     
     
     /*
